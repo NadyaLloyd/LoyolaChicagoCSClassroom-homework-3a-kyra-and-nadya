f7cda3c0ac15214903df4b7449745547f9462649
Nadya Lloyd <nlloydsantiago@luc.edu>
2019-12-03 04:58:08 +0000

    condition annotation

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main()
 
 	for (int i = 0; i < strlen(myString); i++)
 	{ //Start setting up conditions
-		myString[i] = tolower(myString[i]);
+		myString[i] = tolower(myString[i]); //condition for lowercase acception
 		if(!isalpha(myString[i]))
 		{
 			memmove(&myString[i], &myString[i + 1], strlen(myString) - i);
