21031b219f453c5a89f449194b174167a2c7813f
kyrageryol <54647512+kyrageryol@users.noreply.github.com>
2019-12-03 21:47:19 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <ctype.h> 
 
-int main()
+int main(int argc, char *argv[])
 {
 	int flag; //we'll use this as an indicator
 	char myString[255] = "\0"; //keep track of string
 	char word[255] = "\0"; //keep track of word
 	FILE *fp; //file pointer
 
-	printf("Enter a string: "); //user input
+	printf("Enter a word: "); //user input
 	scanf("%s", myString); //read formatted data
 
 	for (int i = 0; i < strlen(myString); i++)
@@ -18,13 +18,14 @@ int main()
 		if(!isalpha(myString[i]))
 		{
 			memmove(&myString[i], &myString[i + 1], strlen(myString) - i);
+			//copies input data to buffer to compare to text file
 			i--;
 		}
 
 	}
 	fp = fopen("engmix.txt", "r");
 
-	while (fscanf(fp, "%s", word) != EOF); //reads user input, specifies input is string
+	while (fscanf(fp, "%s\n", word) != EOF); //reads user input, specifies input is string
 	{
 		for (int i=0; i < strlen(word); i++) //computes length of string
 		{
